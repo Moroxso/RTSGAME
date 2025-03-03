b313c1512b8df9f9aedb81600d08fd0d455fb150
Moroxso <89519694+Moroxso@users.noreply.github.com>
2025-03-03 21:07:57 +0400

    Изменено движение камеры + исправление бага с отображением ресурсов

--- Assets/Script/BaseScriptMain/MovementCamera.cs
+++ Assets/Script/BaseScriptMain/MovementCamera.cs
@@ -5,50 +5,59 @@ using UnityEngine;
 public class MovementCamera : MonoBehaviour
 {
     [SerializeField] private float _speed = 7.5f;
+    [SerializeField] private GameObject SelectionManager;
+    private SelectionManager selectionManager;
     private int _index = 0;
     private Vector3 _position;
     public float mouseSensitivity = 100f; // Чувствительность мыши
     private float xRotation = 44f; // Текущий угол поворота по оси X
     private float yRotation = 0f;
+    private float speedX = 5f;
+    private float speedZ = 5f;
+
+    [SerializeField] private float edgeThreshold = 10f; // Расстояние от края экрана, при котором камера начинает двигаться
 
     void Start()
     {
-
+        selectionManager = SelectionManager.GetComponent<SelectionManager>();
     }
 
     void Update()
     {
         MovementCameraWithMouse();
         SpeedPlus();
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        MoveCameraWithMouseAtEdge();
+    }
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        transform.Translate(_speed * movement * Time.deltaTime);
+    private void RotationXZ()
+    {
+            speedX = (xRotation / 9);
+            speedZ = 10f - speedX;
     }
 
 
     private void SpeedPlus()
     {
-        if(Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            _index = 1;
-            _speed = 25f;
-            Cursor.lockState = CursorLockMode.Locked;
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                _index = 1;
+                _speed = 25f;
+            Cursor.lockState = CursorLockMode.Confined;
         }
-        if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            _index = 0;
-            _speed = 7.5f;
-            Cursor.lockState = CursorLockMode.None;
-        }
+            if (Input.GetMouseButtonUp(1))
+            {
+                _index = 0;
+                _speed = 7.5f;
+                Cursor.lockState = CursorLockMode.None;
+            }
+        
     }
 
     private void MovementCameraWithMouse()
     {
         if (_index == 1)
         {
-
             // Получаем ввод мыши
             float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime * 10f;
             float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime * 10f;
@@ -56,14 +65,54 @@ public class MovementCamera : MonoBehaviour
             // Поворачиваем камеру по вертикали (вверх/вниз)
             yRotation += mouseX;
             xRotation -= mouseY;
-            xRotation = Mathf.Clamp(xRotation, -90f, 90f); // Ограничиваем угол поворота, чтобы камера не переворачивалась
+            xRotation = Mathf.Clamp(xRotation, 0f, 90f); // Ограничиваем угол поворота, чтобы камера не переворачивалась
 
             transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
-
         }
-
     }
 
+    private void MoveCameraWithMouseAtEdge()
+    {
+        // Получаем позицию курсора на экране
+        Vector3 mousePosition = Input.mousePosition;
+        float scrollDelta = Input.mouseScrollDelta.y;
+        RotationXZ();
 
+        // Проверяем, находится ли курсор близко к краям экрана
+        if (mousePosition.x <= edgeThreshold)
+        {
+            // Двигаем камеру влево
+            transform.Translate(Vector3.left * _speed * 10f * Time.deltaTime);
+        }
+        else if (mousePosition.x >= Screen.width - edgeThreshold)
+        {
+            // Двигаем камеру вправо
+            transform.Translate(Vector3.right * _speed * 10f * Time.deltaTime);
+        }
+        if (mousePosition.y <= edgeThreshold)
+        {
+            // Двигаем камеру вниз
+            transform.Translate(Vector3.down * _speed * speedX * Time.deltaTime);
+            transform.Translate(Vector3.back * _speed * speedZ * Time.deltaTime);
+        }
+        else if (mousePosition.y >= Screen.height - edgeThreshold)
+        {
+            // Двигаем камеру вверх
+            transform.Translate(Vector3.up * _speed * speedX * Time.deltaTime);
+            transform.Translate(Vector3.forward * _speed * speedZ * Time.deltaTime);
+        }
 
+        if (scrollDelta != 0) {
+            if (scrollDelta < 0)
+            {
+                // Двигаем камеру вниз
+                transform.Translate(Vector3.back * _speed * 50f * Time.deltaTime);
+            }
+            else if (scrollDelta > 0)
+            {
+                // Двигаем камеру вверх
+                transform.Translate(Vector3.forward * _speed * 50f * Time.deltaTime);
+            }
+        }
+    }
 }
